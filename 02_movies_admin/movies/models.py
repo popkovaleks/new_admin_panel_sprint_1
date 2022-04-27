da06306e88df778e363706c2fe8b9b6592305d19
Aleksandr <popkov.aleks.v@yandex.ru>
2022-04-27 22:44:25 +0500

    Описаны модели жанра и кинопроизведения

--- 02_movies_admin/movies/models.py
+++ 02_movies_admin/movies/models.py
@@ -1,3 +1,58 @@
+import uuid
+
+
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    name = models.CharField('name', max_length=255)
+
+    description = models.TextField('description', blank=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "content\".\"genre"
+
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
+
+    def __str__(self):
+        return self.name
+
+
+class Filmwork(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    title = models.CharField('title', max_length=255)
+
+    description = models.TextField('description', blank=True)
+
+    creation_date = models.DateField('creation_date')
+
+    rating = models.FloatField('rating')
+
+
+    class FilmworkType(models.TextChoices):
+        MOVIE = 'movie'
+        TV_SHOW = 'TV_SHOW'
+
+
+    type = models.CharField(max_length=255, choices=FilmworkType.choices)
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "content\".\"film_work"
+
+        verbose_name = "Кинопроизведение"
+        verbose_name_plural = "Кинопроизведения"
+
+    def __str__(self):
+        return self.title
