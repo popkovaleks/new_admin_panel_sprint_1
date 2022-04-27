16514226bc4b1471e367475d8708f816ba682378
Aleksandr <popkov.aleks.v@yandex.ru>
2022-04-27 22:45:06 +0500

    Модели жанра и кинопроизведения добавлены в админку

--- 02_movies_admin/movies/admin.py
+++ 02_movies_admin/movies/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Genre, Filmwork
 # Register your models here.
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
+    pass
