d00a5f8681a3352f90f3d95fb8476702a074e540
Aleksandr <popkov.aleks.v@yandex.ru>
2022-04-28 09:04:49 +0500

    Модель person добавлена в админку

--- 02_movies_admin/movies/admin.py
+++ 02_movies_admin/movies/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Genre, Filmwork, GenreFilmwork
+from .models import Genre, Filmwork, Person, GenreFilmwork, PersonFilmwork
 # Register your models here.
 
 class GenreFilmworkInline(admin.TabularInline):
     model = GenreFilmwork
+
+
+class PersonFilmworkInline(admin.TabularInline):
+    model = PersonFilmwork
 
 
 @admin.register(Genre)
@@ -13,4 +17,10 @@ class GenreAdmin(admin.ModelAdmin):
 
 @admin.register(Filmwork)
 class FilmworkAdmin(admin.ModelAdmin):
-    inlines = (GenreFilmworkInline,)
+    inlines = (GenreFilmworkInline, PersonFilmworkInline)
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    pass
+
