beea8ae28fd32fdf965bb1b9ec79c08db4a18a34
Aleksandr <popkov.aleks.v@yandex.ru>
2022-05-07 21:00:33 +0500

    Удалены комментарии, жанры теперь показываются в списке кинопроизведений

--- 02_movies_admin/movies/admin.py
+++ 02_movies_admin/movies/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 from .models import Genre, Filmwork, Person, GenreFilmwork, PersonFilmwork
-# Register your models here.
 
 
 class GenreFilmworkInline(admin.TabularInline):
@@ -22,12 +22,25 @@ class GenreAdmin(admin.ModelAdmin):
 class FilmworkAdmin(admin.ModelAdmin):
     inlines = (GenreFilmworkInline, PersonFilmworkInline)
 
-    list_display = ('title', 'type', 'creation_date', 'rating',)
+    list_display = ('title', 'type', 'creation_date', 'rating', 'get_genres')
 
     list_filter = ('type',)
 
     search_fields = ('title', 'description', 'id', )
 
+    list_prefetch_related = ('genres',)
+    def get_queryset(self, request):
+        queryset = (
+            super()
+            .get_queryset(request)
+            .prefetch_related(*self.list_prefetch_related)
+        )
+        return queryset
+
+    def get_genres(self, obj):
+        return ','.join([genre.name for genre in obj.genres.all()])
+
+    get_genres.short_description = _('Film genres')
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
